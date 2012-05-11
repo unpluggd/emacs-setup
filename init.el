ebc295ae20f14e9c9a85cf553a6c1cff9e943238
Phillip Oldham <phillip.oldham@gmail.com>
2012-05-11 11:13:01 +0100

    moved speedbar to an include as it has become less useful to me, added markdown formatting

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 ;(add-to-list 'load-path "~/.emacs.d/icicles")
 (add-to-list 'load-path "~/.emacs.d/plugins")
 (add-to-list 'load-path "~/.emacs.d/plugins/coffee-mode")
+(add-to-list 'load-path "~/.emacs.d/plugins/markdown-mode")
 ;(add-to-list 'load-path "~/.emacs.d/plugins/twittering-mode")
 ;(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
 ;(add-to-list 'load-path "~/.emacs.d/vendor")
@@ -61,57 +62,7 @@
 
 
 ;; -- speedbar --
-(require 'sr-speedbar)
-
-(setq speedbar-frame-parameters
-      '((minibuffer)
-        (width . 40)
-        (border-width . 0)
-        (menu-bar-lines . 0)
-        (tool-bar-lines . 0)
-        (unsplittable . t)
-        (left-fringe . 0)))
-
-(setq speedbar-hide-button-brackets-flag t)
-(setq speedbar-show-unknown-files t)
-(setq speedbar-smart-directory-expand-flag t)
-(setq speedbar-use-images nil)
-;(setq sr-speedbar-auto-refresh nil)
-(setq sr-speedbar-max-width 40)
-;(setq sr-speedbar-right-side nil)
-(setq sr-speedbar-width-console 40)
-
-(when window-system
-  (defadvice sr-speedbar-open (after sr-speedbar-open-resize-frame activate)
-    (set-frame-width (selected-frame)
-                     (+ (frame-width) sr-speedbar-width)))
-  (ad-enable-advice 'sr-speedbar-open 'after 'sr-speedbar-open-resize-frame)
-  
-  (defadvice sr-speedbar-close (after sr-speedbar-close-resize-frame activate)
-    (sr-speedbar-recalculate-width)
-    (set-frame-width (selected-frame)
-                     (- (frame-width) sr-speedbar-width)))
-  (ad-enable-advice 'sr-speedbar-close 'after 'sr-speedbar-close-resize-frame))
-
-(speedbar-add-supported-extension ".js")
-(add-to-list 'speedbar-fetch-etags-parse-list
-             '("\\.js" . speedbar-parse-c-or-c++tag))
-
-(defadvice delete-other-windows (after my-sr-speedbar-delete-other-window-advice activate)
-  "Check whether we are in speedbar, if it is, jump to next window."
-  (let ()
-	(when (and (sr-speedbar-window-exist-p sr-speedbar-window)
-               (eq sr-speedbar-window (selected-window)))
-      (other-window 1)
-	)))
-(ad-enable-advice 'delete-other-windows 'after 'my-sr-speedbar-delete-other-window-advice)
-(ad-activate 'delete-other-windows)
-
-(if window-system
-    (progn
-      (add-hook 'after-init-hook 'sr-speedbar-open)))
-
-;(sr-speedbar-open)
+;(load-file "~/.emacs.d/environ/speedbar.el")
 
 
 ;; -- Undo/Redo --
@@ -239,6 +190,11 @@
 ;  (lambda () (zencoding-mode 1)))
 ;(add-hook 'php-mode-hook 'my-php-mode-hook)
 
+
+;; -- Markdown --
+
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) 
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
 ;; -- YAML --
 
