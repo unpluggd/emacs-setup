d9702d231825b3c4e9e74175943850e99fed66fb
Phillip B Oldham <phillip.oldham@exstatik.com>
2012-06-27 09:51:40 +0100

    adding further extensions to markdown mode

--- init.el
+++ init.el
@@ -121,6 +121,8 @@
 
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) 
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
 ;; -- YAML --
 
