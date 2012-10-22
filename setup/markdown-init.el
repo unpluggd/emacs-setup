865c09f8d0c571b0de82c0848a705dfd9b672b2c
Phillip Oldham <phillip.oldham@gmail.com>
2012-10-22 14:37:43 +0100

    silencing markdown mode as it crashes emacs

--- setup/markdown-init.el
+++ setup/markdown-init.el
@@ -1,8 +1,10 @@
-(packages-install
-    (cons 'markdown-mode marmalade)
-    )
+;; disabling as it breaks with emacs 24.*
 
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) 
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+;(packages-install
+;    (cons 'markdown-mode marmalade)
+;    )
+
+;(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) 
+;(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+;(setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
+;(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
